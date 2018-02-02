83f093f8ea0b49367121b7dd1399285d3e55f912
Pa3Py6aka <alexgref@ya.ru>
2018-02-02 21:40:52 +0300

    Environment update

--- common/config/params.php
+++ common/config/params.php
@@ -1,12 +1,12 @@
 <?php
 return [
-    'adminEmail' => 'admin@naplite.ru',
-    'supportEmail' => 'support@naplite.ru',
+    'adminEmail' => 'admin@na-plite.ru',
+    'supportEmail' => 'support@na-plite.ru',
     'user.passwordResetTokenExpire' => 3600,
     'user.rememberMeDuration' => 3600 * 24 * 30,
     'cookieDomain' => '.na-plite.ru',
-    'frontendHostInfo' => 'http://naplite.ru',
-    'backendHostInfo' => 'http://cp.naplite.ru',
+    'frontendHostInfo' => 'http://na-plite.ru',
+    'backendHostInfo' => 'http://cp.na-plite.ru',
     'maxUploadFileSize' => 1024 * 1024 * 5,
     'maxUploadFileSizeHuman' => '5 Мб',
 ];
--- environments/prod/common/config/params-local.php
+++ environments/prod/common/config/params-local.php
@@ -1,5 +1,7 @@
 <?php
 return [
+    'adminEmail' => 'admin@na-plite.ru',
+    'supportEmail' => 'support@na-plite.ru',
     'cookieDomain' => '.na-plite.ru',
     'frontendHostInfo' => 'http://na-plite.ru',
     'backendHostInfo' => 'http://cp.na-plite.ru',
