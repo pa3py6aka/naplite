63c354edf2d77b3335b58766e089c3f8fc3673f8
Pa3Py6aka <alexgref@ya.ru>
2018-02-03 11:41:42 +0300

    Tags added

--- core/helpers/ContentHelper.php
+++ core/helpers/ContentHelper.php
@@ -12,7 +12,7 @@ class ContentHelper
         return HtmlPurifier::process($content, [
             'AutoFormat.Linkify' => true,
             //'AutoFormat.PurifierLinkify' => true
-            'HTML.Allowed' => 'a[href],b,i,br,strong,ul,ol,li,s,u,p,em,h1,h2,h3,h4,h5,h6,span,font,div,img'
+            'HTML.Allowed' => 'a[href],b,i,br,strong,ul,ol,li,s,u,p,em,h1,h2,h3,h4,h5,h6,span,font,div,img[src]'
         ]);
     }
 
