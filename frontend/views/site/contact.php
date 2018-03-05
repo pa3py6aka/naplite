9be27870e0cb9b97ff4e85e5b8e31052789754fa
Pa3Py6aka <alexgref@ya.ru>
2018-03-05 14:45:50 +0300

    Contact form update

--- frontend/views/site/contact.php
+++ frontend/views/site/contact.php
@@ -1,5 +1,7 @@
 <?php
 
+use widgets\BannerWidget;
+use widgets\RecipeThemesWidget;
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
@@ -9,66 +11,76 @@ use yii\bootstrap\ActiveForm;
 $this->title = 'Связь с администрацией сайта';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="contact-block">
-    <br>
-    <h1><?= $this->title ?></h1>
-
-    <p>Для связи с нами используйте контактную форму ниже:</p>
-
-    <div class="row">
-        <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
-
-        <div class="inputbox">
-            <div class="inputbox_label">
-                <div class="inputbox_label_2col">
-                    <div class="inputbox_label_left">Ваш e-mail:</div>
-                </div>
+<div class="content_left">
+    <div class="textbox">
+        <div class="tac">
+            <div class="breadcump">
+                <a href="/">Главная</a>
             </div>
-            <?= $form->field($model, 'email')
-                ->input('email', ['autofocus' => true, 'class' => 'input_base'])->label(false) ?>
+            <h1><?= $this->title ?></h1>
         </div>
+        <div class="form_center">
+            <div class="row">
+                <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
 
-        <div class="inputbox">
-            <div class="inputbox_label">
-                <div class="inputbox_label_2col">
-                    <div class="inputbox_label_left">Ваше имя:</div>
+                <div class="inputbox">
+                    <div class="inputbox_label">
+                        <div class="inputbox_label_2col">
+                            <div class="inputbox_label_left">Ваш e-mail:</div>
+                        </div>
+                    </div>
+                    <?= $form->field($model, 'email')
+                        ->input('email', ['autofocus' => true, 'class' => 'input_base'])->label(false) ?>
                 </div>
-            </div>
-            <?= $form->field($model, 'name')
-                ->input('text', ['class' => 'input_base'])->label(false) ?>
-        </div>
 
-        <div class="inputbox">
-            <div class="inputbox_label">
-                <div class="inputbox_label_2col">
-                    <div class="inputbox_label_left">Тема сообщения:</div>
+                <div class="inputbox">
+                    <div class="inputbox_label">
+                        <div class="inputbox_label_2col">
+                            <div class="inputbox_label_left">Ваше имя:</div>
+                        </div>
+                    </div>
+                    <?= $form->field($model, 'name')
+                        ->input('text', ['class' => 'input_base'])->label(false) ?>
                 </div>
-            </div>
-            <?= $form->field($model, 'subject')
-                ->input('text', ['class' => 'input_base'])->label(false) ?>
-        </div>
 
-        <div class="inputbox">
-            <div class="inputbox_label">
-                <div class="inputbox_label_2col">
-                    <div class="inputbox_label_left">Текст сообщения:</div>
+                <div class="inputbox">
+                    <div class="inputbox_label">
+                        <div class="inputbox_label_2col">
+                            <div class="inputbox_label_left">Тема сообщения:</div>
+                        </div>
+                    </div>
+                    <?= $form->field($model, 'subject')
+                        ->input('text', ['class' => 'input_base'])->label(false) ?>
                 </div>
+
+                <div class="inputbox">
+                    <div class="inputbox_label">
+                        <div class="inputbox_label_2col">
+                            <div class="inputbox_label_left">Текст сообщения:</div>
+                        </div>
+                    </div>
+
+                    <?= $form->field($model, 'text', ['options' => ['class' => 'inputbox_input']])
+                        ->textarea([
+                            'cols' => 2,
+                            'rows' => 2,
+                            'class' => 'textarea_base',
+                            'placeholder' => ''
+                        ])
+                        ->label(false) ?>
+                </div>
+
+                <div class="add_recipe_bottom">
+                    <a href="javascript:$('#contact-form').submit();" class="b_red"><i class="fa fa-envelope"></i>Отправить</a>
+                </div>
+
+                <?php ActiveForm::end(); ?>
             </div>
-
-            <?= $form->field($model, 'text', ['options' => ['class' => 'inputbox_input']])
-                ->textarea([
-                    'cols' => 2,
-                    'rows' => 2,
-                    'class' => 'textarea_base',
-                    'placeholder' => ''
-                ])
-                ->label(false) ?>
         </div>
-
-        <div class="radio_and_but">
-            <button type="submit" class="radio_and_but_right blind-button"><a type="submit" class="b_red">Отправить</a></button>
-        </div>
-
-        <?php ActiveForm::end(); ?>
     </div>
+</div>
+<div class="content_right">
+    <?= BannerWidget::widget(['type' => BannerWidget::TYPE_RIGHT, 'bannerId' => 'bannerSimple1']) ?>
+
+    <?= RecipeThemesWidget::widget() ?>
 </div>
