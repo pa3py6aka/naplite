38b997a169ecb547a01787f8cd67d8258d0b36dc
Pa3Py6aka <alexgref@ya.ru>
2018-03-07 13:44:47 +0300

    Remove filters from empty pages

--- frontend/views/users/my-cookbook.php
+++ frontend/views/users/my-cookbook.php
@@ -25,42 +25,7 @@ $this->title .= $category ? ' | ' .$category->getHeadingTile() : '';
                 <?php Pjax::begin([
                     'linkSelector' => '.pjax'
                 ]) ?>
-                <div class="sort_panel">
-                    <div class="sort_panel_left">
-                        <div class="inputbox_2_col">
-                            <div class="inputbox_label_left">Показывать:</div>
-                            <div class="inputbox_label_right">
-                                <select name="sort-selector" class="select_base" data-for-link="sort-selector-link">
-                                    <option value="<?= SortHelper::getUrl($provider->sort, 'id') ?>"<?= $provider->sort->getAttributeOrder('id') == SORT_DESC ? ' selected' : '' ?>>Самые новые</option>
-                                    <option value="<?= SortHelper::getUrl($provider->sort, 'rate') ?>"<?= $provider->sort->getAttributeOrder('rate') == SORT_DESC ? ' selected' : '' ?>>Самые популярные</option>
-                                    <option value="<?= SortHelper::getUrl($provider->sort, 'comments_count') ?>"<?= $provider->sort->getAttributeOrder('comments_count') == SORT_DESC ? ' selected' : '' ?>>Самые обсуждаемые</option>
-                                </select>
-                                <a href="#" class="hidden pjax" data-link="sort-selector-link"></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="sort_panel_right">
-                        <div class="inputbox_2_col">
-                            <div class="inputbox_label_left">Категории:</div>
-                            <div class="inputbox_label_right">
-                                <select name="occasion-selector" class="select_base" data-for-link="occasion-selector-link">
-                                    <option value="<?= Url::to(['/users/cookbook', 'id' => $user->id]) ?>">Все рецепты</option>
-                                    <?php foreach ($userCategories as $userCategory): ?>
-                                        <option
-                                                value="<?= Url::to([
-                                                    '/users/cookbook',
-                                                    'id' => $user->id,
-                                                    'category' => $userCategory->slug,
-                                                ]) ?>"
-                                            <?= $category && $category->id == $userCategory->id ? ' selected' : '' ?>
-                                        ><?= $userCategory->name ?></option>
-                                    <?php endforeach; ?>
-                                </select>
-                                <a href="#" class="hidden pjax" data-link="occasion-selector-link"></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+
                 <?php if (!count($provider->models)): ?>
                     <div class="no-counts">
                         <?php if (Yii::$app->user->id === $user->id): ?>
@@ -70,6 +35,42 @@ $this->title .= $category ? ' | ' .$category->getHeadingTile() : '';
                         <?php endif; ?>
                     </div>
                 <?php else: ?>
+                    <div class="sort_panel">
+                        <div class="sort_panel_left">
+                            <div class="inputbox_2_col">
+                                <div class="inputbox_label_left">Показывать:</div>
+                                <div class="inputbox_label_right">
+                                    <select name="sort-selector" class="select_base" data-for-link="sort-selector-link">
+                                        <option value="<?= SortHelper::getUrl($provider->sort, 'id') ?>"<?= $provider->sort->getAttributeOrder('id') == SORT_DESC ? ' selected' : '' ?>>Самые новые</option>
+                                        <option value="<?= SortHelper::getUrl($provider->sort, 'rate') ?>"<?= $provider->sort->getAttributeOrder('rate') == SORT_DESC ? ' selected' : '' ?>>Самые популярные</option>
+                                        <option value="<?= SortHelper::getUrl($provider->sort, 'comments_count') ?>"<?= $provider->sort->getAttributeOrder('comments_count') == SORT_DESC ? ' selected' : '' ?>>Самые обсуждаемые</option>
+                                    </select>
+                                    <a href="#" class="hidden pjax" data-link="sort-selector-link"></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="sort_panel_right">
+                            <div class="inputbox_2_col">
+                                <div class="inputbox_label_left">Категории:</div>
+                                <div class="inputbox_label_right">
+                                    <select name="occasion-selector" class="select_base" data-for-link="occasion-selector-link">
+                                        <option value="<?= Url::to(['/users/cookbook', 'id' => $user->id]) ?>">Все рецепты</option>
+                                        <?php foreach ($userCategories as $userCategory): ?>
+                                            <option
+                                                    value="<?= Url::to([
+                                                        '/users/cookbook',
+                                                        'id' => $user->id,
+                                                        'category' => $userCategory->slug,
+                                                    ]) ?>"
+                                                <?= $category && $category->id == $userCategory->id ? ' selected' : '' ?>
+                                            ><?= $userCategory->name ?></option>
+                                        <?php endforeach; ?>
+                                    </select>
+                                    <a href="#" class="hidden pjax" data-link="occasion-selector-link"></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <?= $this->render('@frontend/views/recipes/recipes-block', ['recipesProvider' => $provider]) ?>
                 <?php endif; ?>
 
