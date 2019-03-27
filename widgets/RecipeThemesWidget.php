92d4b7a450ad38285a01d2c4586965a546ee2394
Pa3Py6aka <alexgref@ya.ru>
2019-03-27 15:53:54 +0300

    Turn off "recipes by theme" widget

--- widgets/RecipeThemesWidget.php
+++ widgets/RecipeThemesWidget.php
@@ -9,6 +9,7 @@ class RecipeThemesWidget extends Widget
 {
     public function run()
     {
-        return $this->render('recipe-themes-block');
+        //ToDO: Виджет "Рецепты по теме"
+        //return $this->render('recipe-themes-block');
     }
 }
